50b9a2cf21f120295b6344f9b3fbf8cb159dc424
Anja Müllner <135255924+AnjaMuellner@users.noreply.github.com>
2024-05-25 14:59:21 +0200

    first draft (#1)

--- pixi_diff_to_markdown/__main__.py
+++ pixi_diff_to_markdown/__main__.py
@@ -1,5 +1,45 @@
+import json
+from typing import Literal
+
+import pydantic
+
+
+class CondaVersion(pydantic.BaseModel):
+    version: str
+    build: str
+
+
+class UpdateSpec(pydantic.BaseModel):
+    before: CondaVersion
+    after: CondaVersion
+    type_: Literal["conda"]
+
+
+def update_spec_to_table_line(x: UpdateSpec, package_name: str) -> str:
+    return f"| {package_name} | {x.before.version} {x.before.build} | {x.after.version} {x.after.build} |"
+
+
+def generate_table(x: dict[str, UpdateSpec]) -> str:
+    header = """| Dependency | Before | After |
+| - | - | - |"""
+    pass
+    lines = []
+    for item in x:
+        line = update_spec_to_table_line(x[item], item)
+        lines.append(line)
+    mystring = "\n".join(lines)
+    table = header + "\n" + mystring
+    return table
+
+
 def main():
-    print("Hello, World!")
+    with open("test.json") as f:
+        data = json.load(f)
+    env_dependencies = data["default"]["linux-64"]
+    env_dependencies = {
+        key: UpdateSpec(**value) for (key, value) in env_dependencies.items()
+    }
+    print(generate_table(env_dependencies))
 
 
 if __name__ == "__main__":
---
+++ specification.md
@@ -0,0 +1,13 @@
+- enable column: change, explicit/implicit, conda/pypi
+
+- split-tables: "no" | "environment" | "platform" (and environment)
+
+- if only one environment, then leave environment out
+  - same for platform
+
+- cursive for explicit/implicit (if no explicit column) (with footnote)
+- footnote if downgrade
+
+environment | dependency | before | after |change | explicit/implicit | pypi/conda
+
+sort by: environment -> platform -> change -> explicit/implicit -> pypi/conda -> alphabet
